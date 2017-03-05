b170e3fd300e8d9180e2ba05d4366b288df7fd7f
87chan <honji31415@gmail.com>
2017-03-06 02:33:56 +0900

    ギミック ・汎用処理を基底に移動

--- SampleGame/Classes/Gimmick/GimmickBase.cpp
+++ SampleGame/Classes/Gimmick/GimmickBase.cpp
@@ -1,4 +1,5 @@
 #include "GimmickBase.h"
+#include "AppHeader.h"
 
 GimmickBase::GimmickBase()
 {}
@@ -6,6 +7,26 @@ GimmickBase::GimmickBase()
 void GimmickBase::Initialize(b2World* world, cocos2d::CCNode* parentNode, const cocos2d::Vec2& pos, float size)
 {
 	MyDrawNode::Initialize(world, parentNode);
+
+	DrawData = cocos2d::CCDrawNode::create();
+	this->addChild(DrawData);
+
+	/* 物理エンジン上の物質の定義 */
+	b2BodyDef rectBodyDef;
+
+	/*
+	物体の力に対する属性
+	b2_dynamicBody: 速度があり力に反応する
+	b2_staticBody: 速度が無く力に反応しない
+	b2_kinematicBody: 速度があり力に反応しない
+	*/
+	rectBodyDef.type = b2_staticBody;
+
+	/* 物理エンジンの空間上の座標 */
+	rectBodyDef.userData = DrawData;
+
+	/* 物理エンジン上の物質作成 */
+	BodyData = world->CreateBody(&rectBodyDef);
 }
 
 void GimmickBase::Finalize(b2World* world, cocos2d::CCNode* parentNode)
--- SampleGame/Classes/Gimmick/GimmickGoal.cpp
+++ SampleGame/Classes/Gimmick/GimmickGoal.cpp
@@ -12,29 +12,7 @@ void GimmickGoal::Initialize(b2World* world, cocos2d::CCNode* parentNode, const 
 {
 	GimmickBase::Initialize(world, parentNode, pos, size);
 
-	DrawData = cocos2d::CCDrawNode::create();
 	DrawData->drawCircle(Vec2::ZERO, size, 0.0f, 64, false, ccColor4F::RED);
-	this->addChild(DrawData);
-
-	/* 物理エンジン上の物質の定義 */
-	b2BodyDef rectBodyDef;
-	/*
-	物体の力に対する属性
-	b2_dynamicBody: 速度があり力に反応する
-	b2_staticBody: 速度が無く力に反応しない
-	b2_kinematicBody: 速度があり力に反応しない
-	*/
-	rectBodyDef.type = b2_staticBody;
-
-	/* 物理エンジンの空間上の座標 */
-	rectBodyDef.userData = DrawData;
-
-	/* 物理エンジン上の物質作成 */
-	BodyData = world->CreateBody(&rectBodyDef);
-	b2MassData massData = b2MassData();
-	massData.center = BodyData->GetWorldCenter();
-	massData.mass = DEFAULT_MASS;
-	BodyData->SetMassData(&massData);
 
 	/* 物理エンジン上の物質の形と大きさ */
 	b2CircleShape spriteShape;
